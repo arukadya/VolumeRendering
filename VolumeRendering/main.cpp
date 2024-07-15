b9515a50f9caed4e7cda177723e200134daa2bbb
Toshiki <toshiki.arukadya@outlook.jp>
2024-07-15 14:47:54 +0900

    Initial Commit

---
+++ VolumeRendering/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  VolumeRendering
+//
+//  Created by 須之内俊樹 on 2024/07/15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
